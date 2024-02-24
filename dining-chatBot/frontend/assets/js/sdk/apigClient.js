9be7775af1a3eb50cb7197d15bcc6f51df6491c3
Rohan <46161834+rohnnie@users.noreply.github.com>
2024-02-23 22:11:09 -0500

    Add files via upload

--- dining-chatBot/frontend/assets/js/sdk/apigClient.js
+++ dining-chatBot/frontend/assets/js/sdk/apigClient.js
@@ -53,7 +53,7 @@ apigClientFactory.newClient = function (config) {
 
     
     // extract endpoint and path from url
-    var invokeUrl = 'https://77th38ozb2.execute-api.us-east-1.amazonaws.com/diningChatBot';
+    var invokeUrl = 'https://51ibk0frya.execute-api.us-east-1.amazonaws.com/cloudAssign1';
     var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
     var pathComponent = invokeUrl.substring(endpoint.length);
 
